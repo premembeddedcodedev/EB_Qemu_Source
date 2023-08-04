bb24b140ea3cc8f1bded7ae5807072e7e9c8e3e2
Prem <95699835+premembeddedcodedev@users.noreply.github.com>
2023-08-04 16:03:10 +0530

    Update mount.sh

--- prepare_drive/mount.sh
+++ prepare_drive/mount.sh
@@ -1,2 +1,11 @@
+#boot folder extract
 sudo losetup /dev/loop35 sd.img -o 1048576 --sizelimit 31456768
 sudo -s mount /dev/loop35 mnt/boot/
+
+#Rootfs folder extract
+sudo losetup /dev/loop36 sd.img -o 32505856 --sizelimit 83885568
+sudo -s mount /dev/loop36 mnt/rootfs/
+
+#media folder extract
+sudo losetup /dev/loop37 sd.img -o 116391936 --sizelimit 17825280
+sudo -s mount /dev/loop37 mnt/media/
