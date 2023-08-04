d2a8ae316a37d7e964fddbea9275cae1cca297e3
premv <prem441@gmail.com>
2023-08-04 19:54:40 +0530

    added run.sh for running entire qemu

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-
+unxz qemu-bin/qemu-system-aarch64.xz
 unxz sd.img.xz
-sudo -s ../qemu_rpi4/build/qemu-system-aarch64 -M raspi3b  -kernel ./images/u-boot.bin -serial null -serial stdio -sd ./sd.img
+sudo -s ./qemu-bin/qemu-system-aarch64 -M raspi3b  -kernel ./images/u-boot.bin -serial null -serial stdio -sd ./sd.img
