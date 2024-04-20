4a4b3ae22b4bd29d439115970c4f6f0d634cfadb
Prem <95699835+premembeddedcodedev@users.noreply.github.com>
2024-04-20 20:43:12 +0530

    Update run_qemu_vexpress.sh

--- vexpress/run_qemu_vexpress.sh
+++ vexpress/run_qemu_vexpress.sh
@@ -2,3 +2,5 @@
 #qemu-system-arm -machine vexpress-a9 -cpu cortex-a9 -m 128M -dtb arch/arm/boot/dts/arm/vexpress-v2p-ca9.dtb -kernel ../u-boot/u-boot -nographic
 #qemu-system-arm -machine vexpress-a9 -cpu cortex-a9 -m 512M -dtb arch/arm/boot/dts/arm/vexpress-v2p-ca9.dtb -kernel ../u-boot/u-boot -sd sd.img -nographic
 qemu-system-arm -machine vexpress-a9 -cpu cortex-a9 -m 1G -dtb arch/arm/boot/dts/arm/vexpress-v2p-ca9.dtb -kernel ../u-boot/u-boot -sd sd.img -nographic -append "root=/dev/sda console=ttyAMA0" -no-reboot -net nic
+#qemu-system-arm -M vexpress-a9 -kernel arch/arm/boot/zImage -nographic
+
