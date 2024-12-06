882981f922efda836de163a7c13064850416138b
Prem <95699835+premembeddedcodedev@users.noreply.github.com>
2024-12-06 11:46:05 +0530

    Update run_qemu.sh

--- riscv/run_qemu.sh
+++ riscv/run_qemu.sh
@@ -1,1 +1,3 @@
 qemu-system-riscv64 -nographic -machine virt -kernel /home/praveenv/sambashare/Src/linux/arch/riscv/boot/Image -initrd /home/praveenv/sambashare/Src/busybox/initramfs/initramfs.cpio.gz   -append "console=ttyS0"
+qemu-system-riscv64 -nographic -machine virt -kernel /home/praveenv/sambashare/EB_Qemu_Source/riscv/Image -append "root=/dev/vda rw console=ttyS0" -drive file=/home/praveenv/sambashare/EB_Qemu_Source/riscv/rootfs.img,format=raw,id=hd0
+
